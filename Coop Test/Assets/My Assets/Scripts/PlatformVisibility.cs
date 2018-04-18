91e7e7062eea91c4d93ad82c2ab9cfef815558b4
husky5114 <thien5114@gmail.com>
2018-04-17 22:06:17 -0700

    prototype

---
+++ Coop Test/Assets/My Assets/Scripts/PlatformVisibility.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlatformVisibility : NetworkBehaviour {
+
+    public bool server = false;
+    public bool client = false;
+    public bool serverVisible = false;
+    public bool clientVisible = false;
+    public bool blueOnly = false;
+    public bool redOnly = false;
+
+    public Material blue;
+    public Material red;
+
+	// Use this for initialization
+	void Start () {
+        if (!((server && isServer) || (client && !isServer))) {
+            gameObject.SetActive(false);
+        }
+        if ((isServer && !serverVisible) || (!isServer && !clientVisible)) {
+            gameObject.GetComponentInChildren<SpriteRenderer>().enabled = false;
+        }
+        if (blueOnly) {
+            gameObject.GetComponentInChildren<SpriteRenderer>().material = blue;
+            SetLayerR(gameObject, 10);
+        }
+        if (redOnly) {
+            gameObject.GetComponentInChildren<SpriteRenderer>().material = red;
+            SetLayerR(gameObject, 11);
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SetLayerR(GameObject obj, int newLayer) {
+        obj.layer = newLayer;
+        foreach(Transform child in obj.transform){
+            if (null == child) {
+                continue;
+            }
+            SetLayerR(child.gameObject, newLayer);
+        }
+    }
+}
---
+++ Coop Test/Assets/My Assets/Scripts/Visibility.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class Visibility : NetworkBehaviour {
+
+    public GameObject lights;
+    public Material blue;
+    public Material red;
+    private Material mat;
+
+	// Use this for initialization
+	void Start () {
+        if (isLocalPlayer) {
+            lights.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        setColor();
+    }
+
+    void onConnected() {
+
+    }
+
+    void setColor() {
+        if (isServer) {
+            if (isLocalPlayer) {
+                gameObject.GetComponent<SpriteRenderer>().material = blue;
+                lights.GetComponent<Light>().color = blue.color;
+            }
+            else {
+                gameObject.GetComponent<SpriteRenderer>().material = red;
+                lights.GetComponent<Light>().color = red.color;
+
+            }
+        }
+        else {
+            if (isLocalPlayer) {
+                gameObject.GetComponent<SpriteRenderer>().material = red;
+                lights.GetComponent<Light>().color = red.color;
+
+            }
+            else {
+                gameObject.GetComponent<SpriteRenderer>().material = blue;
+                lights.GetComponent<Light>().color = blue.color;
+
+            }
+        }
+    }
+}
